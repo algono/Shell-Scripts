0408b9dd08015076bc38b1bf88e52b6961722d63
Alejandro Gómez <algono@inf.upv.es>
2020-02-11 15:11:37 +0100

    Update crear-rama-ut.sh

--- Git/Transportify/crear-rama-ut.sh
+++ Git/Transportify/crear-rama-ut.sh
@@ -6,8 +6,10 @@ then
     exit 1
 fi
 
+read -d '' setVariables << EOF
 $NEW_BRANCH="sprint$1/$2"
 $SPRINT_MASTER_BRANCH="sprint$1/master"
+EOF
 
-git branch $NEW_BRANCH $SPRINT_MASTER_BRANCH 
-git checkout $NEW_BRANCH
+git branch "$NEW_BRANCH" "$SPRINT_MASTER_BRANCH" 
+git checkout "$NEW_BRANCH"
